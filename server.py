a3e34476ed57f60ae828b8f33b46c325661a1a15
Kate Hymers <kate.droukman@gmail.com>
2020-04-06 19:55:34 +1000

    Updating to any URI

--- server.py
+++ server.py
@@ -5,19 +5,14 @@ PORT = 9080
 
 class ServerHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
-        if self.path == "/apm/metricFeed":
-            content_len = int(self.headers.getheader('content-length', 0))
-            post_body = self.rfile.read(content_len)
-            self.send_response(200)
-            self.end_headers()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        self.send_response(200)
+        self.end_headers()
 
-            with open("layer7output.txt", "a") as outfile:
-                outfile.write(post_body + "\n---\n")
-                outfile.close()
-        else:
-            self.send_response(200)
-            self.end_headers()
-        return
+        with open("layer7output_v2.txt", "a") as outfile:
+            outfile.write(self.path + ": " + post_body + "\n---\n")
+            outfile.close()
 
 
 httpd = SocketServer.TCPServer(("", PORT), ServerHandler)
