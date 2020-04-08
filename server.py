f2f8c5d42c0e34c3564a4ee28cc9c63e9a8abb6f
Kate Hymers <kate.droukman@gmail.com>
2020-04-09 09:34:17 +1000

    Updated port number

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
 import time
 
-PORT = 9080
+PORT = 9081
 
 class ServerHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
@@ -11,7 +11,7 @@ class ServerHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-        with open("layer7output_v2.txt", "a") as outfile:
+        with open("layer7output_v3.txt", "a") as outfile:
             outfile.write(self.path + ": " + time.strftime("%Y%m%d-%H%M%S") + post_body + "\n---\n")
             outfile.close()
 
